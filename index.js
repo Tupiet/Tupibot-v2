5d6ddb420d994a2cb473e1872e99340672035d77
Tupiet <62516852+Tupiet@users.noreply.github.com>
2021-11-24 18:23:01 +0100

    Adding test commands

--- index.js
+++ index.js
@@ -7,4 +7,18 @@ client.once('ready', () => {
 	console.log('Hey, I\'m ready!');
 });
 
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	} else if (commandName === 'server') {
+		await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
+	} else if (commandName === 'user') {
+		await interaction.reply('User info.');
+	}
+});
+
 client.login(token);
