f99ba058b7a40e9f654756953f563855215e2505
Tupiet <62516852+Tupiet@users.noreply.github.com>
2021-12-01 14:18:42 +0100

    trying

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ client.on('interactionCreate', async interaction => {
 	} else if (commandName === 'server') {
 		await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
 	} else if (commandName === 'user') {
-		await interaction.reply('User info.');
+		await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
 	}
 });
 
